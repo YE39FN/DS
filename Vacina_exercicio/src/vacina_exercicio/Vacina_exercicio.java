aa6616f51c3a42632805b94824fd7623a1af8ed6
Arthur Baldo Alves <arthurbaldoalves@gmail.com>
2025-05-29 13:44:39 -0300

    Exercício da Vacina em Java

---
+++ Vacina_exercicio/src/vacina_exercicio/Vacina_exercicio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vacina_exercicio;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Vacina_exercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
